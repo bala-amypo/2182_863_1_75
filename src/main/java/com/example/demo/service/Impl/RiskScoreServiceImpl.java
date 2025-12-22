5fa1923a9955ba8a51239cc9690ce89e353673c0
bala-amypo <bala@amypo.in>
2025-12-22 08:26:28 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/RiskScoreServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/RiskScoreServiceImpl.java
@@ -1,4 +1,23 @@
-@Override
-public List<RiskScore> getAllScores() {
-    return repository.findAll();
+package com.example.demo.service.Impl;
+
+import com.example.demo.model.RiskScore;
+import com.example.demo.repository.RiskScoreRepository;
+import com.example.demo.service.RiskScoreService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RiskScoreServiceImpl implements RiskScoreService {
+
+    private final RiskScoreRepository riskScoreRepository;
+
+    public RiskScoreServiceImpl(RiskScoreRepository riskScoreRepository) {
+        this.riskScoreRepository = riskScoreRepository;
+    }
+
+    @Override
+    public List<RiskScore> getAllScores() {
+        return riskScoreRepository.findAll();
+    }
 }
