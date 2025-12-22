71a6cf68db469e6176f6ca41c82cf43f6e81f9e2
bala-amypo <bala@amypo.in>
2025-12-22 02:56:44 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/RiskScoreServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/RiskScoreServiceImpl.java
@@ -1,36 +1,14 @@
-package com.example.demo.service.impl;
+package com.example.demo.service.Impl;
 
-import com.example.demo.model.*;
-import com.example.demo.repository.*;
-import com.example.demo.service.RiskScoreService;
-import com.example.demo.util.RiskLevelUtils;
-import com.example.demo.exception.BadRequestException;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
+import com.example.demo.service.RiskScoreService;
 
 @Service
 public class RiskScoreServiceImpl implements RiskScoreService {
 
-    private final VisitorRepository visitorRepository;
-    private final VisitLogRepository visitLogRepository;
-    private final RiskRuleRepository riskRuleRepository;
-    private final RiskScoreRepository riskScoreRepository;
-    private final ScoreAuditLogRepository scoreAuditLogRepository;
-
-    public RiskScoreServiceImpl(VisitorRepository visitorRepository,
-                                VisitLogRepository visitLogRepository,
-                                RiskRuleRepository riskRuleRepository,
-                                RiskScoreRepository riskScoreRepository,
-                                ScoreAuditLogRepository scoreAuditLogRepository) {
-        this.visitorRepository = visitorRepository;
-        this.visitLogRepository = visitLogRepository;
-        this.riskRuleRepository = riskRuleRepository;
-        this.riskScoreRepository = riskScoreRepository;
-        this.scoreAuditLogRepository = scoreAuditLogRepository;
+    @Override
+    public int calculateRiskScore() {
+        return 0;
     }
 
-    @Override
-    public RiskScore evaluateVisitor(Long visitorId) {
-        Visitor visitor = visitorRe
+}
